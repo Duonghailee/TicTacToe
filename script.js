f2817a92eb417c16d58db46a594e532f8321f62e
Duonghailee <duonghailee@gmail.com>
2017-09-03 01:29:43 +0300

    added extra comment and built a completed stupid, randomly move from computer, fully working now

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-/* 2 players together mode 
+/* there 2 two option modes, 1 player against computure and 2 players
  */
+var mode1 = false,
+    mode2 = false;
 
 /* Use 1 to indicate player 1 and -1 to indicate the other player 
  */
@@ -16,13 +18,15 @@ var x = "<img src='./Images/X.png' width='130' height='100' >",
     isP2 = !isP1;
 
 
-// If player choose X, store choice as value 1, O for value 2    
+// If player choose X, store choice as value 1, 'O' for value 2    
 var isX = 1,
     isY = -1;
 
 /*
 Create new 9-size block array and fill 9 elements with 0s, it is the back-end of game board of 9 blocks*/
 var Arr = Array(3).fill(0).map(() => Array(3).fill("0"));
+
+
 
 /*
 Where to inform winning player */
@@ -34,7 +38,7 @@ $('.x, .o').click(function() {
     isX = $(this).attr('value'); //1 if choose X, -1 if choose O
 
     //Player 1 always start firt
-    $('#turn').html("Player 1 's turn");
+    $('#turn').html("Player 1's turn");
 
     //calling function startGame
     startGame();
@@ -47,6 +51,7 @@ $('.x, .o').click(function() {
 $('#option1').click(function() {
     $('#intro').hide();
     $('#one_player_mode').show();
+    mode1 = true;
 });
 
 /*
@@ -54,6 +59,7 @@ $('#option1').click(function() {
 $('#option2').click(function() {
     $('#intro').hide();
     $('#two_player_mode').show();
+    mode2 = true;
 });
 
 
@@ -64,6 +70,10 @@ $('#back, #reset').click(function() {
     $('.mode').hide();
     $('#game_board').hide();
     clearGameBoard();
+    mode1 = false;
+    mode2 = false;
+    isP1 = true;
+    isP2 = false;
 });
 
 
@@ -79,54 +89,124 @@ function startGame() {
 
 /* clicking start here
  */
-$('.square').click(function() {
-    $(this).attr('disabled', true); //lock further click, once one block is chosen, no any changeable
-    if (isP1) { //If player 1 is currently playing
-        $('#turn').html("Player 2 's turn"); // inform next turn is player 2
-        $(this).attr('value', 1); // add value 1 to the button
-        if (isX == 1) { // if player 1 chooses X
-            isY = -1; //player 2 has to choose Y
-            $(this).html(x); //print X on boardgame for player 1
 
-        } else {
-            $(this).html(o); // else print O
-            isY = 1; //player 2 print X in contrast
+/* 2 players mode */
+
+
+$('button').click(function play() {
+    if (mode2) {
+        $(this).prop('disabled', true); //lock further click, once one block is chosen, no any changeable
+        if (isP1) { //If player 1 is currently playing
+            $('#turn').html("Player 2 's turn"); // inform next turn is player 2
+            $(this).attr('value', 1); // add value 1 to the button
+            if (isX == 1) { // if player 1 chooses X
+                isY = -1; //player 2 has to choose Y
+                $(this).html(x); //print X on boardgame for player 1
+
+            } else {
+                $(this).html(o); // else print O
+                isY = 1; //player 2 print X in contrast
+            }
+            isP1 = false; // Player 1 finished its turn
+        } else { //Let's move to player 2 's turn
+
+            $('#turn').html("Player 1 's turn"); // inform next turn is player 1
+            $(this).attr('value', -1); // add value -1 to the button
+
+            if (isY == 1) { // if player 2 has to choose X because player 1 chosed O
+                $(this).html(x); //print X
+                isX = -1; //player 1 choosed O
+            } else {
+                $(this).html(o);
+                isX = 1; //player 1 choosed X
+            }
+            isP1 = true; //player 1's turn
         }
-        isP1 = false; // Player 1 finished its turn
-    } else { //Let's move to player 2 's turn
 
+
+        /*
+        always checking whether the game has stop due to tie, or winner is found.*/
+        var win = checkWinner(p1);
+        if (win === 1) {
+            winner.innerHTML = "Player 1 win";
+            lockClick(); //lock further clickable button
+
+            return;
+        } else if (win === -1) {
+            winner.innerHTML = "Player 2 win";
+            lockClick();
+            return;
+        } else if (win === 0) {
+            winner.innerHTML = "Game tie";
+        }
+    } else { //mode 1
+        $(this).prop('disabled', true); //lock further click, once one block is chosen, no any changeable
+        var id = parseInt($(this).attr('id').substring(3));
+        console.log(id); //test the position player 1 clicked
+        if (isP1) { //If player 1 is currently playing
+
+            $('#turn').html("Computer's turn"); // inform next turn is player 2
+            window.setTimeout(10);
+            $(this).attr('value', 1); // add value 1 to the button
+            if (isX == 1) { // if player 1 chooses X
+                isY = -1; //computer has to choose Y
+                $(this).html(x); //print X on boardgame for player 1
+
+            } else {
+                $(this).html(o); // else print O
+                isY = 1; //computer print X in contrast
+            }
+            // isP1 = false; // Player 1 finished its turn
+        }
+
+
+        //now Computer turn
+        var pos = 1; //assign default for computer to start from block 1
+
+        //If the next pos is similar to block that player 1 has played, then randomly generate another number
+        while ($('#btn' + pos).prop('disabled') == true) {
+            pos = Math.floor(Math.random() * 9) + 1;
+            console.log("after random: " + pos);
+        }
+
+        var putPos = '#btn' + pos;
+        console.log(putPos); //test the next put mark
+
+
+        //computer making random move
         $('#turn').html("Player 1 's turn"); // inform next turn is player 1
-        $(this).attr('value', -1); // add value -1 to the button
-
-        if (isY == 1) { // if player 2 has to choose X because player 1 chosed O
-            $(this).html(x); //print X
+        $(putPos).attr('value', -1); // add value -1 to the button
+        $(putPos).prop('disabled', true); //lock further click
+        if (isY == 1) { // if computer has to choose X because player 1 chosed O
+            $(putPos).html(x); //print X
             isX = -1; //player 1 choosed O
         } else {
-            $(this).html(o);
+            $(putPos).html(o);
             isX = 1; //player 1 choosed X
         }
-        isP1 = true; //player 1's turn
+        //  isP1 = true; //player 1's turn
+
+        /*
+        always checking whether the game has stop due to tie, or winner is found.*/
+        var win = checkWinner(p1);
+        if (win === 1) {
+            winner.innerHTML = "Player 1 win";
+            lockClick(); //lock further clickable button
+
+            return;
+        } else if (win === -1) {
+            winner.innerHTML = "Computer win";
+            lockClick();
+            return;
+        } else if (win === 0) {
+            winner.innerHTML = "Game tie";
+        }
+
     }
-
-
-    /*
-    always checking whether the game has stop due to tie, or winner is found.*/
-    var win = checkWinner(p1);
-    if (win === 1) {
-        winner.innerHTML = "Player 1 win";
-        lockClick(); //lock further clickable button
-
-        return;
-    } else if (win === -1) {
-        winner.innerHTML = "Player 2 win";
-        lockClick();
-        return;
-    } else if (win === 0) {
-        winner.innerHTML = "Game tie";
-    }
-
 
 });
+
+
 
 
 /* func clear the game board 
@@ -144,7 +224,7 @@ function clearGameBoard() {
  */
 function lockClick() {
     $('.square').each(function() {
-        $(this).attr('disabled', true); //unlock further click
+        $(this).prop('disabled', true); //unlock further click
     });
 }
 
